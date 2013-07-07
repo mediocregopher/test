e0b6e23fd282a78c66eb6f9280465d307a00cd64
Brian Picciano <bgp2396@bellsouth.net>
2013-07-07 11:33:19 -0400

    now with choices

--- snap/manifest.py
+++ snap/manifest.py
@@ -1,5 +1,10 @@
 def run(self):
 
+    self.header("LOOK, A CHOICE")
+    r = self.choice("the choice",
+        {"A":"A","B":"B","C":"C"})
+    self.header("YOU CHOSE {0}".format(r))
+
     self.local_script("test1")
     self.stage('.',['foo'],['bar'])
     self.remote_script("test2")
