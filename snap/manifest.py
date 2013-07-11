b1926aaa39f44a159399773c1c31bbcd83c71e7d
Brian Picciano <bgp2396@bellsouth.net>
2013-07-11 19:12:28 -0400

    testing choice

--- snap/manifest.py
+++ snap/manifest.py
@@ -3,5 +3,8 @@ def run(self):
     fens = self.get_nodes_in_group("front_end_nodes")
 
     self.local_script("test1")
-    self.stage('.',['foo'],['bar'],fens)
-    self.remote_script("test2",fens)
+
+    yn = self.choice("Snap to fens?",False)
+    if yn:
+        self.stage('.',['foo'],['bar'],fens)
+        self.remote_script("test2",fens)
